8c6d597d2a4aa659ef1f258a1c9767b948119a8c
k-shenbagaraj <shenbax@seas.upenn.edu>
2026-08-21 16:12:53 -0400

    removed zenoh and foxglove launching from entrypoint

--- entrypoint.bash
+++ entrypoint.bash
@@ -3,16 +3,7 @@
 source /opt/ros/jazzy/setup.bash
 source /home/dtc/ws/install/setup.bash
 
-if [ "$RMW_IMPLEMENTATION" = "rmw_zenoh_cpp" ]; then
-    echo "[GLIDER] Starting Zenoh router..."
-    ros2 run rmw_zenoh_cpp rmw_zenohd > /tmp/zenoh_router.log 2>&1 &
-    sleep 2
-fi
-
 if [ "$RUN" = "true" ]; then
-    echo "[GLIDER] Starting foxglove_bridge..."
-    nohup ros2 run foxglove_bridge foxglove_bridge --ros-args -p address:='0.0.0.0' -p port:=8765 > /dev/null 2>&1 &
-    sleep 3
     echo "[GLIDER] Launching glider..."
     ros2 launch glider glider-node.launch.py use_sim_time:="${USE_SIM_TIME:-false}"
 else
